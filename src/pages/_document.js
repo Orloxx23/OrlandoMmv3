049a7122fdbe4424ec1d5f687fe76144dbd4a7e4
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-04-12 22:05:06 -0500

    Script component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,12 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="" lang="en">
-      <Head>
-        <script
-          src="https://kit.fontawesome.com/00a734f883.js"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
+      <Head />
       <body className="bg-[#f8efff] dark:bg-[#09030e]">
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ import {
   ThemeCard,
   TwitterCard,
 } from "@/components";
+import Script from "next/script";
 
 export default function Home() {
   return (
@@ -23,6 +24,10 @@ export default function Home() {
         <meta name="description" content="Orlando Mina personal website" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <Script
+          src="https://kit.fontawesome.com/00a734f883.js"
+          crossOrigin="anonymous"
+        ></Script>
       </Head>
       <div className="flex flex-col m-5">
         <div className="grid grid-cols-3 gap-2 md:grid-cols-4 md:gap-4 mt-5 container mx-auto xl:px-20">
@@ -32,11 +37,10 @@ export default function Home() {
           <BirthdayCard />
           <OnlineCard />
           <SpotifyCard />
-          <GithubCard/>
-          <Skills/>
+          <GithubCard />
+          <Skills />
           <TwitterCard />
           <EmailCard />
-          
         </div>
       </div>
     </>
