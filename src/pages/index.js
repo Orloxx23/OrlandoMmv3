a1ed963313a093d432e7893edbe92a88974c1af1
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2023-06-27 20:08:44 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,6 @@ import {
   TimeCard,
   TwitterCard,
 } from "@/components";
-import Script from "next/script";
 import { useTranslation } from "react-i18next";
 
 export default function Home() {
@@ -27,6 +26,51 @@ export default function Home() {
     <>
       <Head>
         <title>{t("title")}</title>
+        <meta name="theme-color" content="#3F275C" />
+        <meta name="apple-mobile-web-app-status-bar" content="#3F275C" />
+        <meta
+          name="description"
+          content="Orlando is a frontend developer with an interest in UX/UI design. He is an expert in HTML, CSS, JavaScript and React."
+        />
+        <meta name="googlebot" content="notranslate" />
+        <meta name="author" content="Orlando Mina" />
+        <link rel="author" href="https://orlandomm.me" />
+        <meta name="generator" content="Next.js" />
+        <meta
+          name="keywords"
+          content="Next.js,React,JavaScript,Orlando,Mina,Madroñero,Madronero,CSS,HTML,Orloxx,Frontend,Developer,Web,Fullstack,"
+        />
+        <meta name="creator" content="Orlando Mina" />
+        <meta
+          name="format-detection"
+          content="telephone=no, address=no, email=no"
+        />
+
+        <meta property="og:title" content="Orlando Mina | Frontend Developer" />
+        <meta
+          property="og:description"
+          content="Orlando is a frontend developer with an interest in UX/UI design. He is an expert in HTML, CSS, JavaScript and React."
+        />
+        <meta property="og:url" content="https://orlandomm.me/" />
+        <meta
+          property="og:site_name"
+          content="Orlando Mina, Frontend Developer"
+        />
+        <meta property="og:locale" content="es_CO" />
+        <meta
+          property="og:image:url"
+          content="https://github.com/Orloxx23/OrlandoMmv2/blob/main/src/assets/img/me.png?raw=true"
+        />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:image:alt" content="Orlando Mina" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@Orloxx23" />
+        <meta name="twitter:creator" content="@Orloxx23" />
+        <meta name="twitter:title" content="Orlando Mina | Frontend Developer" />
+        <meta name="twitter:image" content="https://github.com/Orloxx23/OrlandoMmv2/blob/main/src/assets/img/me.png?raw=true" />
       </Head>
       <motion.div
         initial={{ opacity: 0, y: -2000 }}
@@ -44,12 +88,12 @@ export default function Home() {
           <ProjectsCard />
           <GithubCard />
           <Skills />
-          <EnglishCard/>
+          <EnglishCard />
           <SpotifyCard />
           <OnlineCard />
           <EmailCard />
           <TwitterCard />
-          <TimeCard />          
+          <TimeCard />
           <ClonCard />
           <PlatziCard />
         </div>
