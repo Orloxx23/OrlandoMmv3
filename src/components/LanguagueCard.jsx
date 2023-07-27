3c4d7143651886fc918df2b30c7b992cfdf0dad8
Orloxx23 <madorlix@gmail.com>
2023-07-27 12:50:16 -0500

    Update LanguagueCard.jsx

--- src/components/LanguagueCard.jsx
+++ src/components/LanguagueCard.jsx
@@ -1,4 +1,4 @@
-import moment from "moment";
+/*import moment from "moment";
 import React, { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 
@@ -51,6 +51,57 @@ export default function LanguagueCard() {
       <p className="font-bold transition duration-300 ease-in-out ">
         <span className={language === "en" ? "text-purple-400" : "text-white"}>EN</span>{" "}
         <span className={language === "es" ? "text-purple-400" : "text-white"}>ES</span>
+      </p>
+    </div>
+  );
+}
+*/
+// Test
+import moment from "moment";
+import React, { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+
+export default function LanguagueCard() {
+  const [t, i18n] = useTranslation("global");
+  const [language, setLanguage] = React.useState("en");
+
+  useEffect(() => {
+    const item = localStorage.getItem("language");
+    if (item) {
+      setLanguage(item);
+      moment.locale(item); // Make sure to set the locale for moment.js on initial load.
+      i18n.changeLanguage(item);
+    } else {
+      localStorage.setItem("language", "en");
+      setLanguage("en");
+      moment.locale("en"); // Set the default locale for moment.js
+      i18n.changeLanguage("en");
+    }
+  }, [i18n]);
+
+  const toggleLanguage = () => {
+    const newLanguage = language === "en" ? "es" : "en";
+    setLanguage(newLanguage);
+    moment.locale(newLanguage); // Set moment.js locale when changing the language.
+    i18n.changeLanguage(newLanguage);
+    localStorage.setItem("language", newLanguage); // Update the language in local storage.
+  };
+
+  return (
+    <div
+      onClick={toggleLanguage}
+      className="flex flex-col justify-center items-center bg-[#1c053a9c] dark:bg-[#af72ff56] rounded-3xl relative overflow-hidden col-span-1 aspect-square shadow-sm cursor-pointer hover:scale-[103%] transition duration-300 ease-in-out"
+    >
+      <p className="font-semibold uppercase text-white dark:text-white text-5xl md:text-8xl">
+        {language.toUpperCase()}
+      </p>
+      <p className="font-bold transition duration-300 ease-in-out ">
+        <span className={language === "en" ? "text-purple-400" : "text-white"}>
+          EN
+        </span>{" "}
+        <span className={language === "es" ? "text-purple-400" : "text-white"}>
+          ES
+        </span>
       </p>
     </div>
   );
